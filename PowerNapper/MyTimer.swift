ebeeae7534d65d14f9817fddc04d381cf0cd6420
Cleo "Lo" Howard <lolearningcode@gmail.com>
2019-05-07 10:44:21 -0600

    Created data model for timer

--- PowerNapper/MyTimer.swift
+++ PowerNapper/MyTimer.swift
@@ -7,3 +7,50 @@
 //
 
 import Foundation
+
+class MyTimer: NSObject {
+    
+    var timeRemaining: TimeInterval?
+    var timer: Timer?
+    
+    var isOn: Bool {
+        return timeRemaining != nil
+    }
+    
+    var timeAsString: String {
+        let timeRemaining = Int(self.timeRemaining ?? 20 * 60)
+        let minutesRemaining = timeRemaining / 60
+        let secondsRemaining = timeRemaining - (minutesRemaining * 60)
+        return String(format: "%02d : %02d", arguments: [minutesRemaining, secondsRemaining])
+    }
+    
+    func secondTicked() {
+        guard let timeRemaining = timeRemaining else { return }
+        if timeRemaining > 0 {
+            self.timeRemaining = timeRemaining - 1
+            print(timeRemaining)
+        } else {
+            timer?.invalidate()
+            self.timeRemaining = nil
+        }
+    }
+    
+    func startTimer(time: TimeInterval) {
+        if !isOn {
+            timeRemaining = time
+            DispatchQueue.main.async {
+                self.timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { (_) in
+                    self.secondTicked()
+                })
+            }
+        }
+    }
+    
+    func stopTimer() {
+        if isOn {
+            timeRemaining = nil
+        } 
+    }
+    
+    
+}
